792e7314fbe904b320989a9f9d41c42f89a45917
Demon <115996678+demonawa@users.noreply.github.com>
2022-11-04 12:28:43 +0100

    Add files via upload

---
+++ Programacion/Practica3/Ejercicio 1/TestSemaforo.java
@@ -0,0 +1,27 @@
+import Semaforo.Semaforo;
+
+public class TestSemaforo {
+    public static void main(String[] args) {
+        Semaforo semaforo1 = new Semaforo();
+        Semaforo semaforo2 = new Semaforo();
+
+        semaforo1.setColor("Verde");
+        System.out.println(semaforo1.getColor());
+
+        semaforo2.setColor("Rojo");
+        System.out.println(semaforo2.getColor());
+
+
+        if(semaforo1.getColor().equals("Rojo")){
+            System.out.println("No puedes pasar");}
+
+        if(semaforo1.puedoPasar()==true){
+            System.out.println("Puedes pasar");}
+       
+        else {System.out.println("No puedes pasar");}
+
+        System.out.println("\n"+semaforo1.toString());
+        System.out.println("\n"+semaforo2.toString());
+        
+    }
+}
